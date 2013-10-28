c316addff7ec23178b594fbb3e29aad020079b3c
luoqii <bangbangitudou@gmail.com>
2013-10-28 17:07:54 +0800

    install by InputStream instead of file:///blabla

--- felix-on-android/NAStore/src/net/neosum/android/nastore/gui/FileItems.java
+++ felix-on-android/NAStore/src/net/neosum/android/nastore/gui/FileItems.java
@@ -1,6 +1,8 @@
 package net.neosum.android.nastore.gui;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import net.neosum.android.nastore.R;
@@ -12,8 +14,8 @@ import org.osgi.framework.BundleException;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
@@ -53,7 +55,8 @@ public class FileItems extends ArrayAdapter<FileItem>{
 				public void onClick(View v) {
 					try {
 						System.out.println("file://"+f.getAbsolutePath());
-						Bundle bb = bc.installBundle("file://"+f.getAbsolutePath());
+                        Bundle bb = bc.installBundle(f.getName(), new FileInputStream(f));
+						
 						if(bb != null)
 						{
 							bb.start();
@@ -62,7 +65,10 @@ public class FileItems extends ArrayAdapter<FileItem>{
 					} catch (BundleException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
+					} catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
 				}
              });
              
